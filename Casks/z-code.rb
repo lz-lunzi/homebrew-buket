53dbc67fb1b6b0e6d65d8da502bba62c07617e37
liu <1426531235@qq.com>
2025-12-30 15:08:41 +0800

    build: 更新 Z Code 的 SHA256 校验值

--- Casks/z-code.rb
+++ Casks/z-code.rb
@@ -1,6 +1,6 @@
 cask "z-code" do
   version "0.11.1"
-  sha256 "placeholder_sha256_replace_with_actual_value"
+  sha256 "0cee980de978625430904a1ae70b1ad2695119a2be9b3d86ce756e9bd91e396c"
 
   url "https://cdn.zcode-ai.com/zcode/releases/#{version}/z-code_#{version}_aarch64.dmg"
   name "Z Code"
