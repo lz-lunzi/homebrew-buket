3e51d499554a6314d6a086911c91bad1f743dffe
liu <1426531235@qq.com>
2026-02-24 11:46:42 +0800

    feat: 添加 CodeBuddy Code 的 Homebrew 安装公式

---
+++ Formula/codebuddy-code.rb
@@ -0,0 +1,35 @@
+class CodebuddyCode < Formula
+  desc "Use CodeBuddy, Tencent's AI assistant, right from your terminal"
+  homepage "https://cnb.cool/codebuddy/codebuddy-code"
+  url "https://registry.npmjs.org/@tencent-ai/codebuddy-code/-/codebuddy-code-2.50.7.tgz"
+  sha256 :no_check
+  license "MIT"
+  version "2.50.7"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  def caveats
+    <<~EOS
+      CodeBuddy Code is Tencent's AI assistant for terminal-based development.
+
+      Available commands:
+        cbc          - Short alias for codebuddy
+        codebuddy    - Main command
+
+      Get started:
+        codebuddy --help
+        cbc --help
+
+      Visit https://cnb.cool/codebuddy/codebuddy-code for more information.
+    EOS
+  end
+
+  test do
+    assert_match "codebuddy", shell_output("#{bin}/codebuddy --version")
+  end
+end
