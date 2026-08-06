7a176bff564c044d879364da99fb6c19bb622f29
liu <1426531235@qq.com>
2026-08-06 11:02:30 +0800

    Formula/orca: move URL to top-level to fix tap livecheck on macOS

--- Formula/orca.rb
+++ Formula/orca.rb
@@ -9,14 +9,13 @@ class Orca < Formula
     strategy :github_latest
   end
 
+  url "https://github.com/stablyai/orca/releases/download/v#{version}/orca-ide_#{version}_amd64.deb"
+  sha256 "ecec2fe903e29afd8dea52db68f5a4db28eef077e9aa478061df5a2fdaead521"
+
   on_linux do
     on_arm do
       url "https://github.com/stablyai/orca/releases/download/v#{version}/orca-ide_#{version}_arm64.deb"
       sha256 "fbd792c155064a64d3fe3b015b908e4fa367c7374f9a194f8eaddfe8fe82521d"
-    end
-    on_intel do
-      url "https://github.com/stablyai/orca/releases/download/v#{version}/orca-ide_#{version}_amd64.deb"
-      sha256 "ecec2fe903e29afd8dea52db68f5a4db28eef077e9aa478061df5a2fdaead521"
     end
   end
 
