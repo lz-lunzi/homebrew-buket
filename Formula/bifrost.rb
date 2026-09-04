dd4d935403be67c7a471a7260f881d8240dcbab0
liu <1426531235@qq.com>
2026-09-04 10:51:01 +0800

    feat: add linux/intel support to bifrost, wire into multiarch CI
    - bifrost.rb: add on_intel (macos) + on_linux arm64/amd64 (gnu builds),
      explicit version line required for #{version} interpolation
    - auto-update.yml: route bifrost through update_multiarch.rb

--- Formula/bifrost.rb
+++ Formula/bifrost.rb
@@ -2,6 +2,7 @@ class Bifrost < Formula
   desc "High-performance HTTP/HTTPS/SOCKS5 proxy server written in Rust"
   homepage "https://github.com/bifrost-proxy/bifrost"
   license "MIT"
+  version "0.0.187"
 
   livecheck do
     url :stable
@@ -12,6 +13,21 @@ class Bifrost < Formula
     on_arm do
       url "https://github.com/bifrost-proxy/bifrost/releases/download/v0.0.187/bifrost-v0.0.187-aarch64-apple-darwin.tar.xz"
       sha256 "070e65b5556641498fb1d77115c9a7aa408748f5d670f0fa1bcc07ffd0ea1140"
+    end
+    on_intel do
+      url "https://github.com/bifrost-proxy/bifrost/releases/download/v0.0.187/bifrost-v0.0.187-x86_64-apple-darwin.tar.xz"
+      sha256 "3d077309065beb111520071f0da2b7eca0041af491567528cb25a357878ef05d"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/bifrost-proxy/bifrost/releases/download/v0.0.187/bifrost-v0.0.187-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "2cfa7b1816d2b318adb770cbcca32b8ea8e7f479d6d39e034dc8a623ea1df85a"
+    end
+    on_intel do
+      url "https://github.com/bifrost-proxy/bifrost/releases/download/v0.0.187/bifrost-v0.0.187-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "819551a57481ecfe1cd8a5f2fe2991744878a39f3edc6460fe629f661fadaaee"
     end
   end
 
