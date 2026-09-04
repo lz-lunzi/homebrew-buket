c8cab7a9220b527953623d6bbacc15b829002696
liu <1426531235@qq.com>
2026-09-04 11:22:49 +0800

    fix: restore octopus on_intel/on_linux blocks (lost from stash)
    Previous fix 3517ac6 only committed script + multica-cli; the octopus.rb
    edit stayed in a stash and never landed. Re-add mac-intel + linux
    arm64/amd64 blocks with interpolated URLs.

--- Formula/octopus.rb
+++ Formula/octopus.rb
@@ -15,6 +15,21 @@ class Octopus < Formula
       url "https://github.com/bestruirui/octopus/releases/download/v0.13.2/octopus-darwin-arm64.zip"
       sha256 "bdf046d27371c5886f7f0018fdccdfd602cbf331311c6a4eb44ecff07ab07b44"
     end
+    on_intel do
+      url "https://github.com/bestruirui/octopus/releases/download/v0.13.2/octopus-darwin-amd64.zip"
+      sha256 "6ade5f52e762dc9086ba7fe06d53237595d93372200c767f2aecd9c16989fe96"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/bestruirui/octopus/releases/download/v0.13.2/octopus-linux-arm64.zip"
+      sha256 "b2755f49d26187ca52fd62ba32bd259c11f456fec858baf6bc5b97f143f7fb3d"
+    end
+    on_intel do
+      url "https://github.com/bestruirui/octopus/releases/download/v0.13.2/octopus-linux-amd64.zip"
+      sha256 "30247b2a53856671a333da57236cdef91550a79eb5871c13e883168ff438edab"
+    end
   end
 
   def install
